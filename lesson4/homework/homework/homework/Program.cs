9090e7fe083d9843cdd49955499bf37120d1dffc
Filippov-Nikolay <filippovnikolay195@gmail.com>
2024-10-17 22:36:52 +0300

    hw4

--- lesson4/homework/homework/homework/Program.cs
+++ lesson4/homework/homework/homework/Program.cs
@@ -2,6 +2,28 @@
 //TASK:
 //Пользователь вводит строку с клавиатуры.
 //Необходимо зашифровать данную строку используя шифр Цезаря.
+
+//CODE:
+using System.Text;
+
+char[] character = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+StringBuilder text = new StringBuilder("Hello world");
+
+Console.WriteLine("Изначальный текст: " + text.ToString());
+
+for (int i = 0; i < text.Length; i++) {
+    for (int j = 0, c = 0; j < character.Length; j++, c++) {
+        if (text[i].ToString().ToLower() == character[c].ToString()) {
+            if ((c + 3) >= character.Length) { c = -1; }
+
+            text[i] = character[c + 3];
+            break;
+        }
+    }
+}
+
+Console.WriteLine("Результат: " + text.ToString());
+Console.WriteLine("");
 #endregion
 
 
@@ -18,20 +40,21 @@ static int Deff(int a, int b) { return a - b; }
 
 
 string expression = string.Empty;
+bool isNext = false;
 
 do {
-    Console.Write("Enter exp: ");
+    Console.Write("Введите выражение: ");
     expression = Console.ReadLine();
 
-    if (expression == null) {
-        Console.WriteLine("Вы ввели пустую строку!");
-    }
+    isNext = expression == null;
+
+    if (isNext) { Console.WriteLine("Вы ввели пустую строку!"); }
     Console.WriteLine("");
-} while (expression == null);
+} while (isNext);
 
 // Очистка от лишних пробелов
 expression = expression.Replace(" ", "");
-Console.WriteLine(expression);
+Console.WriteLine("Выражение: " + expression);
 
 // Добавляем символы для разделения на элементы
 expression = expression.Replace("+", "_+_");
@@ -42,31 +65,18 @@ string[] arrayOfExpression = expression.Split(new Char[] {'_'}, StringSplitOptio
 
 //1-1-2-3+10+1
 
-int sum = 0;
+int sum = int.Parse(arrayOfExpression[0]);
 for (int i = 1; i < arrayOfExpression.Length; i++) {
-    if (arrayOfExpression[i] == "+") {
-        if (i == 1) {
-            sum += Sum(int.Parse(arrayOfExpression[i - 1]), int.Parse(arrayOfExpression[i + 1]));
-            Console.WriteLine(int.Parse(arrayOfExpression[i - 1]) + " + " + int.Parse(arrayOfExpression[i + 1]) + " = " + sum);
-        } else {
-            sum += Sum(sum, int.Parse(arrayOfExpression[i + 1]));
-            Console.WriteLine(sum + " + " + int.Parse(arrayOfExpression[i + 1]) + " = " + sum);
-        }
-        Console.WriteLine("TempSum: " + sum);
-    } else if (arrayOfExpression[i] == "-") {
-        if (i == 1) {
-            sum += Deff(int.Parse(arrayOfExpression[i - 1]), int.Parse(arrayOfExpression[i + 1]));
-            Console.WriteLine(int.Parse(arrayOfExpression[i - 1]) + " - " + int.Parse(arrayOfExpression[i + 1]) + " = " + sum);
-        } else {
-            sum += Deff(sum, int.Parse(arrayOfExpression[i + 1]));
-            Console.WriteLine(sum + " - " + int.Parse(arrayOfExpression[i + 1]) + " = " + sum);
-        }
-        Console.WriteLine("TempSum: " + sum);
+    if (arrayOfExpression[i] == "-") {
+        sum = Deff(sum, int.Parse(arrayOfExpression[i + 1]));
+    } else if (arrayOfExpression[i] == "+") {
+        sum = Sum(sum, int.Parse(arrayOfExpression[i + 1]));
     }
 }
 
 // Выводим сумму
-Console.WriteLine("Summa: " + sum);
+Console.WriteLine("Сумма: " + sum);
+Console.WriteLine("");
 #endregion
 
 
@@ -74,6 +84,28 @@ Console.WriteLine("Summa: " + sum);
 //TASK:
 //Пользователь с клавиатуры вводит некоторый текст.
 //Приложение должно изменять регистр первой буквы каждого предложения на букву в верхнем регистре.
+
+text = new StringBuilder();
+
+//string textTemp = Console.ReadLine();
+string textTemp = "Lorem Ipsum  is simply dummy  text of and typesetting industry.lorem of  type and scrambled it to make a type specimen book. it has the remaining essentially unchanged. it the PageMaker including.";
+string result = string.Join(" ", textTemp.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries));
+
+text.AppendLine(result.ToString());
+
+Console.WriteLine("Изначальный текст: ");
+Console.WriteLine(result);
+Console.WriteLine("");
+
+for (int i = 0; i < text.Length; i++) {
+    if (text[i] == '.') {
+        if (text[i + 1] == ' ') { text[i + 2] = char.ToUpper(text[i + 2]); }
+        else { text[i + 1] = char.ToUpper(text[i + 1]); }
+    }
+}
+
+Console.WriteLine("Результат: ");
+Console.WriteLine(text);
 #endregion
 
 
@@ -82,4 +114,37 @@ Console.WriteLine("Summa: " + sum);
 //Создайте приложение, проверяющее текст на недопустимые слова.
 //Если недопустимое слово найдено, оно должно быть заменено на набор символов *.
 //По итогам работы приложения необходимо показать статистику действий.
+
+//CODE:
+string[] forbiddenWords = { "dummy", "text", "lorem", "book" };
+char[] signs = { '.', ',', '-', '(', ')', '?'};
+
+textTemp = "Lorem Ipsum  is simply dummy  text of and typesetting industry.lorem of  type and, scrambled it to make a type specimen book. it has the remaining essentially unchanged. it the PageMaker including.";
+
+for (int i = 0; i < signs.Length; i++) { textTemp = textTemp.Replace(signs[i].ToString(), $"_{signs[i].ToString()}_"); }
+
+string[] worksText = textTemp.Split(new[] { '_', ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+for (int i = 0; i < worksText.Length; i++) {
+    string temp = string.Empty;
+
+    for (int j = 0; j < forbiddenWords.Length; j++) {
+        if (worksText[i].ToLower() == forbiddenWords[j].ToLower()) {
+            for (int k = 0; k < worksText[i].Length; k++) { temp += "*";}
+            worksText[i] = temp;
+        }
+    }
+}
+
+textTemp = string.Empty;
+for (int i = 0; i < worksText.Length; i++) {
+    if (worksText[i] == ".") {
+        textTemp += ".";
+    } else if (worksText[i] == ",") {
+        textTemp += ",";
+    } else {
+        textTemp += i == 0 ? worksText[i] : " " + worksText[i];
+    }
+}
+Console.WriteLine(textTemp);
 #endregion
