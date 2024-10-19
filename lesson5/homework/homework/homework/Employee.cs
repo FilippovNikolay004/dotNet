fc83f216de592f1771a59ec0d5c7be5b8a9b5da5
Filippov-Nikolay <filippovnikolay195@gmail.com>
2024-10-19 18:48:54 +0300

    hw5

--- lesson5/homework/homework/homework/Employee.cs
+++ lesson5/homework/homework/homework/Employee.cs
@@ -5,7 +5,7 @@ using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace EmployeeNamespace {
+namespace EmployeeAndAirplane {
     internal class Employee {
         private string firstName;
         private string lastName;
@@ -16,24 +16,56 @@ namespace EmployeeNamespace {
         private string descOfOfficialDuties;
 
         public void DataEntry() {
-            Console.Write("Введите имя: ");
-            firstName = Console.ReadLine();
+            bool isNext = false;
 
-            Console.Write("Введите фамилию: ");
-            lastName = Console.ReadLine();
+            do {
+                Console.Write("Введите имя: ");
+                firstName = Console.ReadLine();
 
-            Console.Write("Введите дату рождения: ");
-            dateOfBirth = Console.ReadLine();
+                isNext = !(IsLetter(firstName));
+                if (isNext) { Console.WriteLine("Нельзя вводить цифры!\n"); }
+            } while (isNext);
 
-            Console.Write("Введите контактный номер тел.: ");
-            contactPhoneNumber = Console.ReadLine();
+            do {
+                Console.Write("Введите фамилию: ");
+                lastName = Console.ReadLine();
 
-            Console.Write("Введите свою почту: ");
-            email = Console.ReadLine();
+                isNext = !(IsLetter(lastName));
+                if (isNext) { Console.WriteLine("Нельзя вводить цифры!\n"); }
+            } while (isNext);
 
-            Console.Write("Введите вашу должность: ");
-            post = Console.ReadLine();
+            do {
+                Console.Write("Введите дату рождения: ");
+                dateOfBirth = Console.ReadLine();
 
+                isNext = !(IsFormatDate(dateOfBirth));
+                if (isNext) { Console.WriteLine("Неверный формат даты.\nФормат должен быть: дд.мм.гггг\n"); }
+            } while (isNext);
+
+            do {
+                Console.Write("Введите контактный номер тел.: ");
+                contactPhoneNumber = Console.ReadLine();
+
+                isNext = IsLetter(contactPhoneNumber);
+                if (isNext) { Console.WriteLine("Нельзя вводить буквы!\n"); }
+            } while (isNext);
+
+            do {
+                Console.Write("Введите свою почту: ");
+                email = Console.ReadLine();
+
+                isNext = !(IsFormatEmail(email));
+                if (isNext) { Console.WriteLine("Вы ввели неверный формат почты!\n"); }
+            } while (isNext);
+
+            do {
+                Console.Write("Введите вашу должность: ");
+                post = Console.ReadLine();
+
+                isNext = !(IsLetter(post));
+                if (isNext) { Console.WriteLine("Нельзя вводить цифры!\n"); }
+            } while (isNext);
+            
             Console.WriteLine("Опишите свои служебные обязанности: ");
             descOfOfficialDuties = Console.ReadLine();
         }
@@ -49,12 +81,63 @@ namespace EmployeeNamespace {
             Console.WriteLine(descOfOfficialDuties);
         }
 
-        public void SetFirstName(string firstName) { this.firstName = firstName; }
-        public void SetLastName(string lastName) { this.lastName = lastName; }
-        public void SetDateOfBirth(string dateOfBirth) { this.dateOfBirth = dateOfBirth; }
-        public void SetContactPhoneNumber(string contactPhoneNumber) { this.contactPhoneNumber = contactPhoneNumber; }
-        public void SetEmail(string email) { this.email = email; }
-        public void SetPost(string post) { this.post = post; }
+        private bool IsLetter(string value) {
+            for (int i = 0; i < value.Length; i++) {
+                if (value[i] >= '0' && value[i] <= '9') { return false; }
+            }
+
+            return true;
+        }
+
+        private bool IsFormatEmail(string email) {
+            if (email.IndexOf("@") != -1) { return true; }
+            return false;
+        }
+
+        private bool IsFormatDate(string date) {
+            if (date.Length > 10 || date.Length < 10) { return false; }
+            if (date[2] != '.') { return false; }
+            if (date[5] != '.') { return false; }
+
+            string tempNumberOfString = "";
+
+            tempNumberOfString = date[0].ToString();
+            tempNumberOfString += date[1].ToString();
+            if (int.Parse(tempNumberOfString) > 31 || 
+                int.Parse(tempNumberOfString) <= 0) { return false; }
+
+            tempNumberOfString = date[3].ToString();
+            tempNumberOfString += date[4].ToString();
+            if (int.Parse(tempNumberOfString) > 12 || 
+                int.Parse(tempNumberOfString) <= 0) { return false; }
+
+            return true;
+        }
+
+        public void SetFirstName(string firstName) {
+            if (!(IsLetter(this.firstName))) { Console.WriteLine("Нельзя вводить цифры!\n"); return; }
+            this.firstName = firstName;
+        }
+        public void SetLastName(string lastName) {
+            if (!(IsLetter(lastName))) { Console.WriteLine("Нельзя вводить цифры!\n"); return; }
+            this.lastName = lastName;
+        }
+        public void SetDateOfBirth(string dateOfBirth) {
+            if (!(IsFormatDate(dateOfBirth))) { Console.WriteLine("Неверный формат даты.\nФормат должен быть: дд.мм.гггг\n"); return; }
+            this.dateOfBirth = dateOfBirth;
+        }
+        public void SetContactPhoneNumber(string contactPhoneNumber) {
+            if (IsLetter(contactPhoneNumber)) { Console.WriteLine("Нельзя вводить буквы!\n"); return; }
+            this.contactPhoneNumber = contactPhoneNumber;
+        }
+        public void SetEmail(string email) {
+            if (!(IsFormatEmail(email))) { Console.WriteLine("Вы ввели неверный формат почты!\n"); return; }
+            this.email = email;
+        }
+        public void SetPost(string post) {
+            if (!(IsLetter(post))) { Console.WriteLine("Нельзя вводить цифры!\n"); return; }
+            this.post = post;
+        }
         public void SetDescOfOfficialDuties(string descOfOfficialDuties) { this.descOfOfficialDuties = descOfOfficialDuties; }
 
         public string GetFirstName() { return this.firstName; }
@@ -65,5 +148,94 @@ namespace EmployeeNamespace {
         public string GetPost() { return this.post; }
         public string GetDescOfOfficialDuties() { return this.descOfOfficialDuties; }
 
+    }
+
+    internal class Airplane {
+        private string name;
+        private string manufacturer;
+        private int yearOfManufacture;
+        private string aircraftType;
+
+        public Airplane() {
+            name = "";
+            manufacturer = "";
+            yearOfManufacture = 0;
+            aircraftType = "";
+        }
+
+        public Airplane(string name, string manufacturer, int yearOfManufacture, string aircraftType) {
+            this.name = name;
+            this.manufacturer = manufacturer;
+
+            if (!IsFormatYear(yearOfManufacture)) {
+                throw new ArgumentException("Неверный формат года!"); 
+            }
+            this.yearOfManufacture = yearOfManufacture;
+
+            if (!IsLetter(aircraftType)) {
+                throw new ArgumentException("Нельзя вводить цифры в тип самолёта!");
+            }
+            this.aircraftType = aircraftType;
+        }
+
+        public Airplane(string name, string manufacturer, int yearOfManufacture) : this(name, manufacturer, yearOfManufacture, "Неизвестно") { }
+
+        public void DataEntry() {
+            bool isNext = false;
+
+            Console.Write("Введите название самолета: ");
+            name = Console.ReadLine();
+
+            Console.Write("Введите название производителя: ");
+            manufacturer = Console.ReadLine();
+
+            do {
+                Console.Write("Введите год выпуска: ");
+                yearOfManufacture = int.Parse(Console.ReadLine());
+
+                isNext = !(IsFormatYear(yearOfManufacture));
+                if (isNext) { Console.WriteLine("Неверный формат года!\n"); }
+            } while (isNext);
+
+            do {
+                Console.Write("Введите тип самолёта: ");
+                aircraftType = Console.ReadLine();
+
+                isNext = !(IsLetter(aircraftType));
+                if (isNext) { Console.WriteLine("Нельзя вводить цифры!\n"); }
+            } while (isNext);
+        }
+
+        public void DataPrint() {
+            Console.WriteLine("Название самолета: " + name);
+            Console.WriteLine("Производитель: " + manufacturer);
+            Console.WriteLine("Год выпуска: " + yearOfManufacture);
+            Console.WriteLine("Тип самолёта: " + aircraftType);
+        }
+
+        private bool IsFormatYear(int year) {
+            if (year.ToString().Length > 4) { return false; }
+            if (year <= 0) { return false; }
+
+            return true;
+        }
+        private bool IsLetter(string value) {
+            for (int i = 0; i < value.Length; i++) {
+                if (value[i] >= '0' && value[i] <= '9') { return false; }
+            }
+
+            return true;
+        }
+    
+        public void SetName(string name) { this.name = name; }
+        public void SetManufacturer(string manufacturer) { this.manufacturer = manufacturer; }
+        public void SetYearOfManufacture(int yearOfManufacture) {
+            if (!(IsFormatYear(yearOfManufacture))) { Console.WriteLine("Неверный формат года!\n"); return; }
+            this.yearOfManufacture = yearOfManufacture;
+        }
+        public void SetAircraftType(string aircraftType) {
+            if (!(IsLetter(aircraftType))) { Console.WriteLine("Нельзя вводить цифры!\n"); }
+            this.aircraftType = aircraftType;
+        }
     }
 }
--- lesson5/homework/homework/homework/Program.cs
+++ lesson5/homework/homework/homework/Program.cs
@@ -1,5 +1,11 @@
-﻿using EmployeeNamespace;
+﻿using EmployeeAndAirplane;
 
 Employee employee = new Employee();
 employee.DataEntry();
 employee.DataPrint();
+
+Airplane airplane = new Airplane("F-35А", "Lockheed Martin Corporation", 2011, "Истребитель");
+airplane.DataPrint();
+
+airplane = new Airplane("F-35А", "Lockheed Martin Corporation", 2011);
+airplane.DataPrint();
