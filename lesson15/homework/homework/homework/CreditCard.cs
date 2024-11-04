e049ac30070e065b78e40e0a4f9d8cab6e296b90
Filippov-Nikolay <filippovnikolay195@gmail.com>
2024-11-04 12:45:01 +0200

    hw-15

---
+++ lesson15/homework/homework/homework/CreditCard.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework {
+    internal class CreditCard {
+        public string CardNumber { get; set; }
+        public string FirstNamePerson { get; set; }
+        public string LastNamePerson { get; set; }
+        public DateTime CardValidityPeriod { get; set; }
+        public int PIN { get; set; }
+        public int CreditLimit { get; set; }
+        public int TheAmountMoney { get; set; }
+
+        public CreditCard() : this (null, null, null, 0, 0, 0) { }
+        public CreditCard(string cardNumber, string firstNamePerson, string lastNamePerson, int pIN, int creditLimit, int theAmountMoney) {
+            CardNumber = cardNumber;
+            FirstNamePerson = firstNamePerson;
+            LastNamePerson = lastNamePerson;
+            CardValidityPeriod = DateTime.Now.AddYears(10);
+            PIN = pIN;
+            CreditLimit = creditLimit;
+            TheAmountMoney = theAmountMoney;
+        }
+
+
+        // Метод для ввода PIN
+        public bool EnterPin() {
+            int numberAttempts = 0;
+            bool isNext = false;
+
+            do {
+                Console.Write("Введите PIN: ");
+                int tempPIN = int.Parse(Console.ReadLine());
+
+                isNext = !(tempPIN == PIN);
+
+                if (isNext) { numberAttempts++; Console.WriteLine("Неверный PIN!\n"); }
+                if (numberAttempts == 3) { Console.WriteLine("Карта заблокирована!"); return false; };
+            } while (isNext);
+
+            Console.WriteLine("PIN верный!");
+            return true;
+        }
+
+        // Метод для вывода информации
+        public void Print() {
+            Console.Write($"Номер каты: ");
+            for (int i = 0; i < CardNumber.Length; i++) {
+                if (i < 13) { Console.Write($"*{ (((i + 1) % 4) == 0 ? " " : "") }"); } 
+                else { Console.Write(CardNumber[i]); }
+            }
+            Console.WriteLine($"\nИмя: {FirstNamePerson}");
+            Console.WriteLine($"Фамилия: {LastNamePerson}");
+            Console.WriteLine($"Срок карты: {CardValidityPeriod}");
+            Console.WriteLine("PIN карты: ****");
+            Console.WriteLine($"Кредитный лимит: {CreditLimit}");
+            Console.WriteLine($"Счёт: {TheAmountMoney}");
+        }
+
+        // Метод для пополнения счёта
+        public void DepositAmount(int amount) {
+            if (amount <= 0) { Console.WriteLine("Введите значение больше 0"); return; }
+            TheAmountMoney += amount;
+        }
+
+        // Метод для расхода денег со счёта
+        public void WithdrawAmount(int amount) {
+            if (amount > TheAmountMoney) { Console.WriteLine("Вы ввели значение превышающий ваш счёт"); }
+            else if (amount <= 0) { Console.WriteLine("Введите значение больше 0"); return; }
+            TheAmountMoney -= amount;
+        }
+
+        // Метод для старта использования кредитных денег
+        public void StartCreditUsage() {
+            Console.WriteLine("Вы начали использовать кредитные средства.");
+        }
+
+        // Метод для достижения заданной суммы денег
+        public bool ReachTargetAmount(int target) {
+            if (TheAmountMoney >= target) {
+                Console.WriteLine("Целевая сумма достигнута!");
+                return true;
+            }
+
+            Console.WriteLine("Целевая сумма пока не достигнута.");
+            return false;
+        }
+
+        // Метод для смены PIN
+        public void ChangePin() { 
+            if (!EnterPin()) { return; }
+
+            bool isNext = false;
+            string tempPIN = null;
+
+            do {
+                Console.Write("Введите новый PIN: ");
+                tempPIN = Console.ReadLine();
+
+                isNext = !(tempPIN.Length > 0 && tempPIN.Length <= 4);
+
+                if (isNext) { Console.WriteLine("Макс. размер пин-кода 4 цифры"); }
+            } while (isNext);
+
+            PIN = int.Parse(tempPIN);
+        }
+    }
+}
---
+++ lesson15/homework/homework/homework/Program.cs
@@ -0,0 +1,168 @@
+﻿using homework;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Program {
+    delegate int[] MyDelegate(int[] a);
+    delegate void MyDelegate1();
+    delegate double MyDelegateFigureArea(double width, double height);
+
+    class MainClass {
+        #region Задание1
+        // Метод для получения всех четных чисел в массиве
+        public static int[] GetEvenNumbers(int[] a) {
+            int[] tempResult = new int[a.Length];
+            int count = 0;
+
+            for (int i = 0, j = 0; i < a.Length; i++) {
+                if ((a[i] % 2) == 0) { tempResult[j++] = a[i]; count++; };
+            }
+
+            int[] result = new int[count];
+            for (int i = 0; i < count; i++) { result[i] = tempResult[i]; }
+
+            return result;
+        }
+
+        // Метод для получения всех нечетных чисел в массиве
+        public static int[] GetOddNumbers(int[] a) {
+            int[] tempResult = new int[a.Length];
+            int count = 0;
+
+            for (int i = 0, j = 0; i < a.Length; i++) {
+                if ((a[i] % 2) != 0) { tempResult[j++] = a[i]; count++; };
+            }
+
+            int[] result = new int[count];
+            for (int i = 0; i < count; i++) { result[i] = tempResult[i]; }
+
+            return result;
+        }
+
+        // Метод для получения всех простых чисел в массиве
+        public static int[] GetPrimeNumbers(int[] a) {
+            int[] tempResult = new int[a.Length];
+            int count = 0;
+
+            for (int i = 0; i < a.Length; i++) {
+                if (IsPrime(a[i])) { tempResult[count++] = a[i]; }
+            }
+
+            int[] result = new int[count];
+            for (int i = 0; i < count; i++) { result[i] = tempResult[i]; }
+
+            return result;
+        }
+        private static bool IsPrime(int number) {
+            if (number <= 1) return false;
+            if (number == 2) return true;
+
+            for (int i = 2; i * i <= number; i++) {
+                if (number % i == 0) { return false; }
+            }
+
+            return true;
+        }
+
+        // Метод для получения всех чисел Фибоначчи в массиве
+        public static int[] GetFibonacciNumbers(int[] a) {
+            int[] tempResult = new int[a.Length];
+            int count = 0;
+
+            for (int i = 0; i < a.Length; i++) {
+                if (IsFibonacci(a[i])) {
+                    tempResult[count++] = a[i];
+                }
+            }
+
+            int[] result = new int[count];
+            for (int i = 0; i < count; i++) {
+                result[i] = tempResult[i];
+            }
+
+            return result;
+        }
+
+        private static bool IsFibonacci(int number) {
+            if (number < 0) return false;
+
+            // Проверка: число является Фибоначчи, если   или 5*n^2 - 4 — это полный квадрат
+            int x1 = 5 * number * number + 4;
+            int x2 = 5 * number * number - 4;
+
+            return IsPerfectSquare(x1) || IsPerfectSquare(x2);
+        }
+
+        private static bool IsPerfectSquare(int num) {
+            int s = (int)Math.Sqrt(num);
+            return s * s == num;
+        }
+
+
+        public static void Print(int[] arr) {
+            for (int i = 0; i < arr.Length; i++) {
+                Console.Write(arr[i] + " ");
+            }
+        }
+        #endregion
+
+        #region Задание2
+        // Метод для отображения текущего времени
+        public static void DisplayCurrentTime() {
+            DateTime date = DateTime.Now;
+            Console.WriteLine(date.ToLongTimeString());
+        }
+
+        // Метод для отображения текущей даты
+        public static void DisplayCurrentDate() {
+            DateTime date = DateTime.Now;
+            Console.WriteLine(date.ToShortDateString());
+        }
+
+        // Метод для отображения текущего дня недели
+        public static void DisplayCurrentDayOfWeek() {
+            DateTime date = DateTime.Now;
+            Console.WriteLine(date.DayOfWeek);
+        }
+
+        // Метод для подсчёта площади треугольника
+        public static double CalculateTriangleArea(double baseLength, double height) { return (baseLength * height) / 2; }
+
+        // Метод для подсчёта площади прямоугольника
+        public static double CalculateRectangleArea(double width, double height) { return width * height; }
+        #endregion
+
+        #region Задание3
+        
+        #endregion
+        static void Main() {
+            MyDelegate[] dg = { GetEvenNumbers, GetOddNumbers, GetPrimeNumbers, GetFibonacciNumbers };
+            MyDelegate1[] dg1 = { DisplayCurrentDate, DisplayCurrentDayOfWeek };
+            Action action = new Action(DisplayCurrentTime);
+            MyDelegateFigureArea[] dgFigureArea = { CalculateTriangleArea, CalculateRectangleArea };
+            Random rnd = new Random();
+
+            int[] arr = new int[15];
+
+            for (int i = 0; i < arr.Length; i++) { arr[i] = rnd.Next(1, 50); }
+            
+            Console.WriteLine("Task 1:");
+            for (int i = 0; i < dg.Length; i++) { Print(dg[i](arr)); Console.WriteLine(""); }
+
+            Console.WriteLine("\nTask 2:");
+            action();
+            for (int i = 0; i < dg1.Length; i++) { dg1[i](); }
+            Console.WriteLine(dgFigureArea[0](6, 4));
+            Console.WriteLine(dgFigureArea[1](6, 4));
+
+
+            // TASK 3
+            Console.WriteLine("\nTask 3:");
+            CreditCard creditCard = new CreditCard("4149567823645978", "Иван", "Иванов", 1234, 50000, 10000);
+            creditCard.Print();
+
+            creditCard.EnterPin();
+            creditCard.ChangePin();
+            creditCard.EnterPin();
+        }
+    }
+}
