d6e0bb8d9b247b5cd561941ff3c270b29b222026
Filippov-Nikolay <filippovnikolay195@gmail.com>
2024-11-05 00:30:31 +0200

    hw-16

---
+++ lesson16/homework/homework/homework/Backpack.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework {
+    delegate void ItemAdd(BackpackItem item);
+    internal class BackpackItem {
+        public string Name { get; set; }
+        public int Volume { get; set; }
+
+        public BackpackItem(string name, int volume) {
+            Name = name;
+            Volume = volume;
+        }
+    }
+    internal class Backpack {
+        public string Color { get; set; }
+        public string Manufacturer { get; set; }
+        public string Fabric { get; set; }
+        public int Weight { get; set; }
+        public int MaxVolume { get; set; }
+        public BackpackItem[] Contents { get; private set; }
+
+        public Backpack() : this(null, null, null, 0, 0, null) { }
+        public Backpack(string theColorTheBackpack, string manufacturerName, string backpackFabric, 
+                        int backpackWeight, int theMaxVolumeTheBackpack, BackpackItem[] theContentsTheBackpack) {
+            Color = theColorTheBackpack;
+            Manufacturer = manufacturerName;
+            Fabric = backpackFabric;
+            Weight = backpackWeight;
+            MaxVolume = theMaxVolumeTheBackpack;
+            Contents = theContentsTheBackpack;
+        }
+
+        public void AddItem(BackpackItem item) {
+            ItemAdd itemAdd = delegate (BackpackItem item) {
+                int currentVolume = 0;
+                for (int i = 0; i < Contents.Length; i++) { currentVolume += Contents[i].Volume; }
+
+                if ((currentVolume + item.Volume) <= MaxVolume) {
+                    BackpackItem[] tempContents = new BackpackItem[Contents.Length + 1];
+                    for (int i = 0; i < Contents.Length; i++) { tempContents[i] = Contents[i]; }
+
+                    tempContents[Contents.Length] = item;
+                    Contents = tempContents;
+
+                    Console.WriteLine($"Элемент '{item.Name}' добавлен в рюкзак.");
+                } else {
+                    throw new Exception("Не удается добавить элемент. Недостаточно места.");
+                }
+            };
+            itemAdd?.Invoke(item);
+        }
+    }
+}
---
+++ lesson16/homework/homework/homework/Program.cs
@@ -0,0 +1,117 @@
+﻿using homework;
+using System.Text.RegularExpressions;
+
+class MainClass {
+    delegate void GetRainbowColorRGB(string color);
+    delegate int CountMultiplesOfSeven(int[] arr);
+    delegate int CountPositiveNumbers(int[] arr);
+    delegate int CountUniqueNegativeNumbers(int[] arr);
+    delegate bool ContainsWord(string text, string word);
+
+    static void Main() {
+        // Task 1
+        GetRainbowColorRGB getRainbowColorRGB = delegate (string color) {
+            bool isFound = true;
+            string[] colors = new string[7] { "red", "orange", "yellow", "green", "cyan", "blue", "purple"};
+            int[][] colorsRGB = new int[][] {
+                new int[] { 255, 0, 0 },
+                new int[] { 255, 127, 0 },
+                new int[] { 255, 255, 0 },
+                new int[] { 0, 255, 0 },
+                new int[] { 0, 0, 255 },
+                new int[] { 75, 0, 130 },
+                new int[] { 143, 0, 255 }
+            };
+
+            for (int i = 0; i < colors.Length; i++) {
+                if (colors[i] == color.ToLower()) {
+                    Console.Write("RGB: (");
+                    for (int j = 0; j < colorsRGB[i].Length; j++) {
+                        Console.Write($"{colorsRGB[i][j] + (j != colorsRGB[i].Length - 1 ? ", " : "")}");
+                    }
+                    Console.WriteLine(")");
+                    isFound = true;
+                    break;
+                }
+                if (i == colors.Length - 1) { isFound = false; }
+            }
+
+            if (!isFound) { Console.WriteLine("Цвет не найден!"); }
+            
+        };
+        getRainbowColorRGB("red");
+        getRainbowColorRGB("red1");
+
+
+        // Task 2
+        BackpackItem waterBottle = new BackpackItem("Бутылка воды", 1);
+        BackpackItem notebook = new BackpackItem("Тетрадть", 1);
+        BackpackItem laptop = new BackpackItem("Ноутбук", 5);
+        BackpackItem pc = new BackpackItem("Компьютер", 10);
+        BackpackItem pen = new BackpackItem("Ручка", 1);
+
+        BackpackItem[] initialContents = { waterBottle, notebook };
+
+        Backpack myBackpack = new Backpack("Чёрный", "Nike", "Nylon", 500, 10, initialContents);
+        try {
+            myBackpack.AddItem(pen);
+            myBackpack.AddItem(pc);
+        }
+        catch (Exception ex) { Console.WriteLine(ex.Message); }
+
+
+        // Task 3
+        Random rnd = new Random();
+        int[] arr = new int[15];
+
+        for (int i = 0; i < arr.Length; i++) { arr[i] = rnd.Next(-20, 20); }
+
+        for (int i = 0; i < arr.Length; i++) {
+            Console.Write($"{arr[i].ToString() + ((i + 1) != arr.Length ? ", " : "\n")}");
+        }
+
+        CountMultiplesOfSeven countMultiplesOfSeven = arr => {
+            int count = 0;
+            for (int i = 0; i < arr.Length; i++) { if ((arr[i] % 7) == 0) { count++; } }
+            return count;
+        };
+        Console.WriteLine($"Кол-во чисел кратных семи: {countMultiplesOfSeven(arr).ToString()}");
+
+        // Task 4
+        CountPositiveNumbers countPositiveNumbers = arr => {
+            int count = 0;
+            for (int i = 0; i < arr.Length; i++) { if (arr[i] > 0) { count++; } }
+            return count;
+        };
+        Console.WriteLine($"Кол-во положительных чисел: {countPositiveNumbers(arr).ToString()}");
+
+        // Task 5
+        CountUniqueNegativeNumbers countUniqueNegativeNumbers = arr => {
+            int count = 0;
+            for(int i = 0; i < arr.Length; i++) {
+                bool isUnique = true;
+                for (int j = 0; j < i; j++) { 
+                    if (arr[i] == arr[j] || arr[i] > 0) { isUnique = false; break; };
+                }
+                if (isUnique) { count++; }
+            }
+            return count;
+        };
+        Console.WriteLine($"Кол-во уникальных отрицательных чисел: {countUniqueNegativeNumbers(arr).ToString()}");
+
+        // Task 6
+        ContainsWord containsWord = (text, word) => {
+            // Удаляем все символы, кроме букв и пробелов
+            string cleanedInput = Regex.Replace(text, @"[^\w\s]", "");
+
+            // Разбиваем строку на элементы массива и убираем пробелы
+            string[] words = cleanedInput.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            for (int i = 0; i < words.Length; i++) { if (words[i].ToLower() == word.ToLower()) { return true; } }
+            return false;
+        };
+        const string word = "Lorem";
+        string text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
+        Console.WriteLine($"Слово: '{word}' {(containsWord(text, word) ? "есть" : "нет")} в тексте");
+    }
+}
