7591a890057c7e45bd827918d11dc6fb1d6d79a1
Filippov-Nikolay <filippovnikolay195@gmail.com>
2024-11-06 20:32:14 +0200

    hw-17

---
+++ lesson17/homework/homework/homework/Program.cs
@@ -0,0 +1,14 @@
+﻿using homework;
+
+VoidSourceEvent v = new VoidSourceEvent();
+BoolSourceEvent b = new BoolSourceEvent();
+
+CreditCard creditCard = new CreditCard("4149567823645978", "Иван", "Иванов", 1234, 50000, 10000);
+
+v.Event += creditCard.Print;
+b.Event += creditCard.EnterPin;
+v.Event += creditCard.ChangePin;
+b.Event += creditCard.EnterPin;
+
+v.GeneratorEvent();
+b.GeneratorEvent();
